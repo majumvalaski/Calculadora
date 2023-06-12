f4d50f4ae08703b80a4ddd6f017682b5b25cb541
Maria Julia <maju.m.valaski@gmail.com>
2023-06-12 09:09:37 -0300

    ajustando js para 5 casas decimais

--- responsive.js
+++ responsive.js
@@ -7,25 +7,29 @@ seletor.addEventListener("change", calcular);
 campo1.addEventListener("keyup", calcular);
 campo2.addEventListener("keyup", calcular);
 
-function calcular(){
-    if (campo1.value==='' || campo2.value===''){
-        resposta.innerHTML ="Campo Vazio";
-        setTimeout(()=>{
-            resposta.innerHTML='';
-        },5000); //o tempo depois que a mensagem some (5000ms = 5s)
-    }
-    else{
+function calcular() {
+    let resultado;
+
+    if (campo1.value === "" || campo2.value === "") {
+        resposta.innerHTML = "Campo Vazio";
+        setTimeout(() => {
+            resposta.innerHTML = "";
+        }, 5000); //o tempo depois que a mensagem some (5000ms = 5s)
+    } else {
         const valor1 = parseInt(campo1.value);
         const valor2 = parseInt(campo2.value);
         const operacao = seletor.value;
-        if (operacao =="somar")
-            resposta.innerHTML = valor1 + valor2;
-        if (operacao =="subtrair")
-            resposta.innerHTML = valor1 - valor2;
-        if (operacao =="multiplicar")
-            resposta.innerHTML = valor1 * valor2;
-        if (operacao =="dividir")
-            resposta.innerHTML = valor1 / valor2; 
+        if (operacao === "somar")
+            resultado = valor1 + valor2;
+        if (operacao === "subtrair")
+            resultado = valor1 - valor2;
+        if (operacao === "multiplicar")
+            resultado = valor1 * valor2;
+        if (operacao === "dividir")
+            resultado = valor1 / valor2;
     }
-                    
+
+    if (resultado !== undefined) {
+        resposta.innerHTML = resultado.toFixed(5);
+    }
 }
